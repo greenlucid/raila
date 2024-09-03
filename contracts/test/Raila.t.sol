3a4b4603feb5d3b478c451545bbedb63db65374a
Green <40367733+greenlucid@users.noreply.github.com>
2024-09-03 21:26:49 +0200

    test: createRequest tests
    2 failing tests and one success test
    
    makes me realize, contract might require
    more main settings, as users can put
    bizarre request parameters which would
    never be accepted in practice

--- contracts/test/Raila.t.sol
+++ contracts/test/Raila.t.sol
@@ -6,6 +6,7 @@ import "forge-std/Test.sol";
 import {IProofOfHumanity} from "../src/IProofOfHumanity.sol";
 import {Raila} from "../src/Raila.sol";
 import {ERC20} from "@openzeppelin-contracts/token/ERC20/ERC20.sol";
+import {UD60x18, powu} from "@prb-math/src/UD60x18.sol";
 
 contract MockPoH is IProofOfHumanity {
     function humanityOf(address owner) public pure returns (bytes20) {
@@ -37,5 +38,33 @@ contract Constructor is Test {
         assertEq(address(raila.USD()), address(usd));
         assertEq(address(raila.PROOF_OF_HUMANITY()), address(poh));
         assertEq(raila.feeRate(), 500);
+    }
+}
+
+contract CreateRequest is Test {
+    IProofOfHumanity poh;
+    ERC20 usd;
+    Raila raila;
+
+    function setUp() public {
+        poh = new MockPoH();
+        usd = new PolloCoin(1e18);
+        raila = new Raila(address(1000), 86400, usd, poh, 500);
+    }
+
+    function testFail_RequesterMustBeHuman() public {
+        vm.prank(address(1336));
+        raila.createRequest(100, UD60x18.wrap(100), 100, "");
+    }
+
+    function test_RequesterMustBeHuman() public {
+        vm.prank(address(1337));
+        raila.createRequest(100, UD60x18.wrap(100), 100, "");
+    }
+
+    function testFail_MaxOneRequestPerHuman() public {
+        vm.prank(address(1337));
+        raila.createRequest(100, UD60x18.wrap(100), 100, "");
+        raila.createRequest(100, UD60x18.wrap(100), 100, "");
     }
 }
